27688807d1ac3eebd33f5d383ad0d671aea549b2
abdullah77669 <154595040+abdullah77669@users.noreply.github.com>
2026-08-19 18:49:23 +0500

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+
+      Dictionary<char,int> freq1=new Dictionary<char,int>();
+
+      Dictionary<char,int> freq2=new Dictionary<char,int>();
+      if(s.Length!=t.Length)
+      return false;
+      for(int i=0;i<s.Length;i++)
+      {
+        if(freq1.ContainsKey(s[i]))
+        {
+            freq1[s[i]]++;
+        }
+        else
+        {
+            freq1.Add(s[i],1);
+        }
+        if(freq2.ContainsKey(t[i]))
+        {
+            freq2[t[i]]++;
+        }
+        else
+        {
+            freq2.Add(t[i],1);
+        }
+
+      }
+      foreach(var c in freq1)
+      {
+        if(freq2.ContainsKey(c.Key))
+        {
+            freq1[c.Key]-=freq2[c.Key];
+        }
+      }
+      foreach(var a in freq1)
+      {
+        if(freq1[a.Key]!=0)
+        return false;
+      }
+      return true;
+     /* bool same= freq1.Count== freq2.Count && freq1.All(x=> freq2.TryGetValue(x.Key, out var value) && value==x.Value);
+      return same;
+*/
+    }
+}
