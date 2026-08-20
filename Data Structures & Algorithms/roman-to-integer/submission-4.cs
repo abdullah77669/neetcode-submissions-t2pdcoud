85e50c88be058fd178dfadcb7554c8caeefd97d9
abdullah77669 <154595040+abdullah77669@users.noreply.github.com>
2026-08-20 12:25:51 +0500

    Add: roman-to-integer - submission-4

---
+++ Data Structures & Algorithms/roman-to-integer/submission-4.cs
@@ -0,0 +1,58 @@
+public class Solution {
+    public int RomanToInt(string s) {
+        
+        Dictionary<char,int> roman=new Dictionary<char,int>{
+            {'M',1000},
+            {'D',500},
+            {'C',100},
+            {'L',50},
+            {'X',10},
+            {'V',5},
+            {'I',1}
+        };
+        int result=0;
+        for(int i=0;i<s.Length;i++)
+        {
+            int temp=roman[s[i]];
+          /*  if(s[i]=='X' && s.Length>i+1 && s[i+1]=='L')
+            {
+                temp=40;
+                i++;
+            }
+            else if(s[i]=='X' && s.Length>i+1 && s[i+1]=='C')
+           { 
+            temp=90;
+            i++;
+           }
+           else if(s[i]=='C' && s.Length>i+1 && s[i+1]=='D')
+           {
+            temp=400;
+            i++;
+           }
+            else if(s[i]=='C' && s.Length>i+1 && s[i+1]=='M')
+           {
+            temp=900;
+            i++;
+           }
+           else if(s[i]=='I' && s.Length>i+1 && s[i+1]=='V')
+           {
+            temp=4;
+            i++;
+           }
+           else if(s[i]=='I' && s.Length>i+1 && s[i+1]=='X')
+           {
+            temp=9;
+            i++;
+           }*/
+           if(s.Length>i+1 && temp<roman[s[i+1]])
+           {
+            result-=temp;
+           }
+           else
+           {
+           result+=temp; 
+           }
+        }
+        return result;
+    }
+}
