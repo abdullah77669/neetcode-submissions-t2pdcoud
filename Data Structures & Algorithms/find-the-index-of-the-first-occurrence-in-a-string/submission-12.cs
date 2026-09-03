ffb1c77a6ed48e7c3b801f0bb01e599928821750
abdullah77669 <154595040+abdullah77669@users.noreply.github.com>
2026-09-03 15:35:52 +0500

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-12

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-12.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        for (int i=0;i<haystack.Length;i++)
+        {
+            
+                int temp=i,a=0;
+                while(temp<haystack.Length && a<needle.Length && haystack[temp]==needle[a])
+                {
+                    if(a==needle.Length-1 )
+                    return i;
+                    temp++;
+                    a++;
+                } 
+            
+        }
+        return -1;
+    }
+}
