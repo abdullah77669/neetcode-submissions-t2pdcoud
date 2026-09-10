c00137dd54c877a6f578330fbadb10ffbf218b79
abdullah77669 <154595040+abdullah77669@users.noreply.github.com>
2026-09-10 13:47:31 +0500

    Add: string-compression - submission-24

---
+++ Data Structures & Algorithms/string-compression/submission-24.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public int Compress(char[] chars) {
+        
+        if(chars.Length==1)
+        return 1;
+
+        string result="";
+        char a=chars[0];
+        result+=a;        
+        int repeat=1;
+        
+        int write=1;
+
+        for(int i=1;i<chars.Length;i++)
+        {
+            if(chars[i]==a)
+            repeat++;
+
+            else if(repeat>1)
+            {
+
+                foreach(char c in repeat.ToString())
+                {
+                chars[write++]=c;
+                }
+                a=chars[i];
+                chars[write++]=chars[i];
+                repeat=1;
+                
+            }
+            else
+            {
+                chars[write++]=chars[i];
+                a=chars[i];
+                repeat=1;
+            }
+
+        }
+        if(repeat!=1)
+        {
+            foreach(char c in repeat.ToString())
+            chars[write++]=c;
+        }
+        
+        return write;
+
+    }
+}
