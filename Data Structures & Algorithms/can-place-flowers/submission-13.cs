8b43f34d98e4dca3ce801e2c05f4aceaf49dc006
abdullah77669 <154595040+abdullah77669@users.noreply.github.com>
2026-09-12 23:50:34 +0500

    Add: can-place-flowers - submission-13

---
+++ Data Structures & Algorithms/can-place-flowers/submission-13.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        
+        if(n==0)
+        return true;
+        if(flowerbed.Length==1 && flowerbed[0]==0 && n==1)
+        return true;
+        if(n==flowerbed.Length)
+        return false;
+
+        int comp=0,zCount=0;
+        for(int i=0;i<flowerbed.Length;i++)
+        {
+            if(flowerbed[i]==0)
+            {
+                bool left=(i==0 || flowerbed[i-1]==0);
+                bool right=(i==flowerbed.Length-1 || flowerbed[i+1]==0);
+                if(left && right)
+                {flowerbed[i]=1;
+                    
+                n--;
+
+                if(n==0)
+                return true;
+                }   
+                
+               
+            }
+        }
+        return false;
+    }
+}
