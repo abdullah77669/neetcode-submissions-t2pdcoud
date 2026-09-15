7be9f4944beb9f1ee72ece096f881f65ec21e87a
abdullah77669 <154595040+abdullah77669@users.noreply.github.com>
2026-09-15 11:59:10 +0500

    Add: max-consecutive-ones - submission-5

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-5.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int temp=0,result=0;
+
+        for(int i=0;i<nums.Length;i++)
+        {
+            if(nums[i]==1)
+            {
+                temp++;
+                result=Math.Max(result,temp);
+            }
+
+            else
+            {
+              //  result=Math.Max(temp,result);
+                temp=0;
+            }
+        }
+        return result;
+    }
+}
