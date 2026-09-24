2445a215b478daf57f7c65034070ef5ac4fe5cdc
abdullah77669 <154595040+abdullah77669@users.noreply.github.com>
2026-09-24 12:39:44 +0500

    Add: three-integer-sum - submission-16

---
+++ Data Structures & Algorithms/three-integer-sum/submission-16.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        List<List<int>> result = new List<List<int>>();
+        Array.Sort(nums);
+        int n = nums.Length;
+
+        for (int i = 0; i < n - 2; i++) {
+            // skip duplicate values for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                    // skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
